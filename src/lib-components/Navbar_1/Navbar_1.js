b5c531830cc6f53a1ae9c2e904491ee8330290e1
Anurag Parashar <73629391+AnuragParashar2000@users.noreply.github.com>
2021-12-11 00:53:45 +0530

    Create Navbar_1.js

---
+++ src/lib-components/Navbar_1/Navbar_1.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import "./Navbar_1.module.css";
+import { MdReorder } from 'react-icons/md';
+import { MdSearch } from 'react-icons/md';
+
+function Navbar() {
+  const [showLinks, setShowLinks] = useState(false);
+
+  return (
+    <div className="Navbar">
+      <div className="leftSide">
+        <div className="links" id={showLinks ? "hidden" : ""}>
+          <a href="/home">Home</a>
+          <a href="/aboutus">About Us</a>
+          <a href="/contact">Contact Us</a>
+          <a href="/career">Careers</a>
+        </div>
+        <button onClick={() => setShowLinks(!showLinks)}><MdReorder/></button>
+      </div>
+      <div className="rightSide">
+        <input type="text" placeholder="Search..." />
+        <button><MdSearch/></button>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
