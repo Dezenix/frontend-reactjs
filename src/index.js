99ae79f126e72fdd0a50f46f6f99a2d29dac564e
Anurag Parashar <73629391+AnuragParashar2000@users.noreply.github.com>
2021-12-11 00:59:23 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export { default as Login } from './lib-components/login'
 export { default as Register } from './lib-components/register'
 export { default as ProductCard } from './lib-components/product_card'
+export { default as Navbar_1} from './lib-components/Navbar_1'
 
