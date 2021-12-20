32ef959d0114071c04f094a8dec86dc34ddb9167
Ehtesham Siddiqui <siddiquie.ehtesham@gmail.com>
2021-12-20 18:12:22 +0530

    Reusable Button Component

--- example/src/components/Home.js
+++ example/src/components/Home.js
@@ -1,27 +1,27 @@
 import React from 'react'
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div className="Home">
-      <div className="header">
+    <div className='Home'>
+      <div className='header'>
         <h1>Frontend-reactjs</h1>
       </div>
 
-      <div className="contri">
-        <div className="contri_heading">
+      <div className='contri'>
+        <div className='contri_heading'>
           <h3>Start Contributing</h3>
         </div>
         <hr />
 
-        <div className="contri_components">
-          <Link to="/login" className="component">
+        <div className='contri_components'>
+          <Link to='/login' className='component'>
             <h2>Login</h2>
           </Link>
-          <Link to="/register" className="component">
+          <Link to='/register' className='component'>
             <h2>Register</h2>
           </Link>
-          <Link to="/product_card" className="component" >
+          <Link to='/product_card' className='component'>
             <h2>Product card</h2>
           </Link>
           {/* Add new Link Here for your new component */}
---
+++ src/lib-components/button/Button.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './button.module.css'
+
+const Button = ({ name }) => {
+  return <button class='btn'>{name}</button>
+}
+
+export default Button
