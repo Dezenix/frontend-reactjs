95cacf2451e1b5d3a8f6367e9ebde84ccf73d051
Sahil Gupte <sahilsachingupte@gmail.com>
2021-12-21 17:14:13 +0530

    Fix prettier config and format
    Signed-off-by: Sahil Gupte <sahilsachingupte@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 export { default as Login } from './lib-components/login'
 export { default as Register } from './lib-components/register'
 export { default as ProductCard } from './lib-components/product_card'
-
